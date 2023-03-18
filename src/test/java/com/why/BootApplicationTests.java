dd0c008b7e922c38a19c6f1878069e695963b7a0
why <1095727927@qq.com>
2023-03-19 00:05:45 +0800

    redis update shopping

--- src/test/java/com/why/BootApplicationTests.java
+++ src/test/java/com/why/BootApplicationTests.java
@@ -1,16 +1,14 @@
 package com.why;
 
-import com.why.reggie.common.R;
 import com.why.reggie.service.CategoryService;
 import com.why.reggie.service.DishService;
 import com.why.reggie.service.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Arrays;
+import java.io.*;
+import java.util.UUID;
 
 @SpringBootTest
 class BootApplicationTests {
@@ -25,13 +23,9 @@ class BootApplicationTests {
     private CategoryService categoryService;
 
     @Test
-    void R() {
-        String st = "16286621587052953611";
-        String str = "1,2,3,4";
-        String[] arr = str.split(",");
-        for (String i : arr) {
-            System.out.println(i);
-        }
+    void a(){
+        System.out.println(123);
     }
-
 }
+
+
